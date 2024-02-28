6abdc6b965e40398d7922e8557d9cc2fb6ee1f3c
Mayya <majko2594@gmail.com>
2024-02-29 02:00:27 +0300

    modified Presenter class

--- Seminar_7_HW/Presenter/Presenter.java
+++ Seminar_7_HW/Presenter/Presenter.java
@@ -18,7 +18,6 @@ public class Presenter<T extends OperationModel> {
 
     public void startOperation()  {
         CalculatorLogger logger = new CalculatorLogger();
-        String txt = "";
         try {
             ComplexNumber num1 = v.inputNumber.apply("Введите первое комплексное число (действительную и мнимую часть через пробел): ");
             ComplexNumber num2 = v.inputNumber.apply("Введите второе комплексное число (действительную и мнимую часть через пробел): ");
@@ -27,26 +26,29 @@ public class Presenter<T extends OperationModel> {
                 case 1: {
                     OperationSum sum = new OperationSum();
                     ComplexNumber result = sum.result(num1, num2);
-                    txt = logger.getExpression(num1, num2, result, choice);
+                    logger.writeFile(logger.getExpression(num1, num2, result, choice), "Seminar_7_HW/Logger/Log.txt");
                     System.out.println(result);
                     break;
                 }
                 case 2: {
                     OperationMultiply multiply = new OperationMultiply();
                     ComplexNumber result = multiply.result(num1, num2);
-                    txt = logger.getExpression(num1, num2, result, choice);
+                    logger.writeFile(logger.getExpression(num1, num2, result, choice), "Seminar_7_HW/Logger/Log.txt");
                     System.out.println(result);
                     break;
                     }
                 case 3: {
-                    OperationDivide divide = new OperationDivide();
-                    ComplexNumber result = divide.result(num1, num2);
-                    txt = logger.getExpression(num1, num2, result, choice);
-                    System.out.println(result);
-                    break;
+                    try {
+                        OperationDivide divide = new OperationDivide();
+                        ComplexNumber result = divide.result(num1, num2);
+                        logger.writeFile(logger.getExpression(num1, num2, result, choice), "Seminar_7_HW/Logger/Log.txt");
+                        System.out.println(result);
+                        break;
+                    } catch (ArithmeticException e) {
+                        System.out.println("Деление на ноль запрещено!");
+                    }
                 }
             }
-            logger.writeFile(txt, "Seminar_7_HW/Logger/Log.txt");
         } catch (InputMismatchException e) {
             System.out.println("Значения введены некорректно, повторите ввод");
         }
